892711d57081498ca0b318acccd0e244d944f5db
oscxr <oruiz5761@gmail.com>
2025-12-15 12:17:12 -0600

    Cambio de reverb a pusher

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,41 +1,20 @@
-import axios from 'axios';
 import Echo from 'laravel-echo';
-
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
 
 const API_HOST = import.meta.env.VITE_API_HOST;
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
-    authorizer: (channel) => {
-        return {
-            authorize: (socketId, callback) => {
-                const token = localStorage.getItem('api_token');
-
-                if (!token) {
-                    callback(new Error('No autenticado'));
-                    return;
-                }
-
-                axios.post(`${API_HOST}/broadcasting/auth`, {
-                    socket_id: socketId,
-                    channel_name: channel.name
-                }, {
-                    headers: {
-                        'Authorization': `Bearer ${token}`,
-                        'Accept': 'application/json'
-                    }
-                })
-                    .then(response => callback(null, response.data))
-                    .catch(error => callback(error));
-            }
-        };
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+    authEndpoint: `${API_HOST}/broadcasting/auth`,
+    auth: {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('api_token')}`,
+            Accept: 'application/json'
+        }
     }
 });
